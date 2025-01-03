cf2d5ebc13974be0315ea31dc8d5b3e2e837d473
Mehrsa <mehrsaaa.161@gmail.com>
2025-01-03 06:34:25 +0330

    database upload page and analyzer added.

--- extractor/views.py
+++ extractor/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import pandas as pd
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'extractor/index.html')
+
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES['file']:
+        file = request.FILES['file']
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        file_path = fs.path(filename)
+
+        try:
+            data = pd.read_csv(file_path)
+            columns = data.columns.tolist()
+            return render(request, 'extractor/upload_success.html', {'columns': columns})
+        except Exception as e:
+            return render(request, 'extractor/upload_error.html', {'error': str(e)})
+
+    return render(request, 'extractor/upload.html')
